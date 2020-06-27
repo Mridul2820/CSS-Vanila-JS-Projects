366fc772053db222ac43f7356daab71aa46aad3e
Mridul2820 <immridulpanda2820@gmail.com>
2020-06-27 18:10:15 +0530

    <title>Fluid Image Pop Up</title>

---
+++ Vanila JS/fluid-image-popup/script.js
@@ -0,0 +1,25 @@
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.gallery img');
+const original = document.querySelector('.full-img');
+const caption = document.querySelector('.caption');
+
+previews.forEach(preview => {
+    preview.addEventListener('click', () => {
+        modal.classList.add('open');
+        original.classList.add('open');
+
+        // Dynamic change of text and image 
+        const originalSrc = preview.getAttribute('data-original');
+        original.src = `./assets/full/${originalSrc}`;
+
+        const altText = preview.alt;
+        caption.innerHTML = altText;
+    })
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal') ){
+        modal.classList.remove('open');
+        original.classList.remove('open');
+    }
+})
